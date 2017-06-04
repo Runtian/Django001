c4931d95d1e3d8fe62ac4a512ec689e485584e71
Runtian Liu <curlylrt@Runtians-MacBook-Pro.local>
2017-06-04 11:14:37 +0800

    fix some bug

--- mysite/accountant/admin.py
+++ mysite/accountant/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+import logging
+logger = logging.getLogger("django.server")
 from django.contrib import admin
 
 # Register your models here.
@@ -8,13 +11,18 @@ from .models import BurdenSheet, ProductOrder, ProductName, Customer, CustomerPr
 
 
 class ProductOrderInline(admin.TabularInline):
+    verbose_name = "公司备料"
     model = ProductOrder
     #readonly_fields = ('account_payable',)
-    extra = 5
+    extra = 3
+    exclude = ('date',)
+
 
 class CustomerProductInline(admin.TabularInline):
+    verbose_name = "客户来料"
     model = CustomerProduct
-    extra = 5
+    extra = 3
+    exclude = ('date',)
 
 
 class BurdenSheetAdmin(admin.ModelAdmin):
@@ -27,6 +35,37 @@ class BurdenSheetAdmin(admin.ModelAdmin):
 
     class Media:
         js = ("js/auto_fill.js",)
+
+    def update_burden_sheet(self, obj):
+        product_orders = ProductOrder.objects.filter(burden_sheet=obj)
+        customer_products = CustomerProduct.objects.filter(burden_sheet=obj)
+        total_discount = 0
+        total_payable = 0
+        total_amount = 0
+        for product_order in product_orders:
+            total_discount += product_order.discount
+            total_payable += product_order.account_payable
+            total_amount += product_order.amount
+
+        for customer_product in customer_products:
+            total_amount += customer_product.amount
+        obj.total_discount = total_discount
+        obj.total_amount = total_amount
+        obj.total_payable = total_payable
+        obj.save()
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for obj in formset.deleted_objects:
+            obj.delete()
+        obj = form.instance
+        for instance in instances:
+            instance.date = obj.date
+            instance.save()
+        formset.save_m2m()
+        if formset.deleted_objects:
+            self.update_burden_sheet(obj)
+
 
 admin.site.register(BurdenSheet, BurdenSheetAdmin)
 admin.site.register(ProductName)
--- mysite/accountant/models.py
+++ mysite/accountant/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
 import uuid
+import logging
+logger = logging.getLogger("django.server")
 
 
 from django.db import models
@@ -9,56 +12,85 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Customer(models.Model):
-	name = models.CharField("姓名", max_length=200, primary_key=True)
+    name = models.CharField("姓名", max_length=200, primary_key=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class ProductName(models.Model):
-	name = models.CharField("名称", max_length=200, primary_key=True)
+    name = models.CharField("名称", max_length=200, primary_key=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class BurdenSheet(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	customer = models.ForeignKey(Customer, verbose_name="客户", max_length=200, blank=False)
 
-	description = models.CharField("摘要", max_length=200, blank=False)
-	total_amount = models.FloatField("投料总量", blank=False)
-	total_payable = models.FloatField("应付合计", blank=False)
-	total_discount = models.FloatField("折扣合计", blank=False)
-	date = models.DateField("生产日期", blank=False)
-	time_stamp = models.DateTimeField("time stamp", auto_now_add=True)
+    initial_date = None
 
-	def __str__(self):
-		return self.description
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, verbose_name="客户", max_length=200, blank=False)
+
+    description = models.CharField("摘要", max_length=200, blank=False)
+    total_amount = models.FloatField("投料总量", blank=False)
+    total_payable = models.FloatField("应付合计", blank=False)
+    total_discount = models.FloatField("折扣合计", blank=False, default=0)
+    date = models.DateField("生产日期", blank=False)
+    time_stamp = models.DateTimeField("time stamp", auto_now_add=True)
+
+    def __init__(self, *args, **kwargs):
+        super(BurdenSheet, self).__init__(*args, **kwargs)
+        self.initial_date = self.date
+
+    def date_changed(self):
+        if self.initial_date != self.date:
+            return True
+        return False
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        if self.date_changed():
+            # date changed - do something here
+            product_orders = ProductOrder.objects.filter(burden_sheet=self)
+            customer_products = CustomerProduct.objects.filter(burden_sheet=self)
+            for product_order in product_orders:
+                product_order.date = self.date
+                product_order.save()
+            for customer_product in customer_products:
+                customer_product.date = self.date
+                customer_product.save()
+
+        super(BurdenSheet, self).save(force_insert, force_update, *args, **kwargs)
+        self.initial_date = self.date
+
+    def __str__(self):
+        return self.description
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class ProductOrder(models.Model):
-	name = models.ForeignKey(ProductName, verbose_name="名称", null=True, on_delete=models.SET_NULL, blank=False)
-	burden_sheet = models.ForeignKey(BurdenSheet, on_delete=models.CASCADE, blank=False)
-	amount = models.FloatField("重量", blank=False)
-	price = models.FloatField("单价", blank=False)
-	discount = models.FloatField("优惠", blank=False)
-	account_payable = models.FloatField("应付款", blank=False)
+    name = models.ForeignKey(ProductName, verbose_name="名称", null=True, on_delete=models.SET_NULL, blank=False)
+    burden_sheet = models.ForeignKey(BurdenSheet, on_delete=models.CASCADE, blank=False)
+    amount = models.FloatField("重量", blank=False)
+    price = models.FloatField("单价", blank=False)
+    discount = models.FloatField("优惠", blank=False)
+    account_payable = models.FloatField("应付款", blank=False)
+    date = models.DateField("生产日期", blank=False, default=datetime.date.today)
 
-	def __str__(self):
-		return self.name.name
+    def __str__(self):
+        return self.name.name
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class CustomerProduct(models.Model):
-	name = models.ForeignKey(ProductName, verbose_name="名称", null=True, on_delete=models.SET_NULL, blank=False)
-	burden_sheet = models.ForeignKey(BurdenSheet, on_delete=models.CASCADE, blank=False)
-	amount = models.FloatField("重量", blank=False)
+    name = models.ForeignKey(ProductName, verbose_name="名称", null=True, on_delete=models.SET_NULL, blank=False)
+    burden_sheet = models.ForeignKey(BurdenSheet, on_delete=models.CASCADE, blank=False)
+    amount = models.FloatField("重量", blank=False)
+    date = models.DateField("生产日期", blank=False, default=datetime.date.today)
 
-	def __str__(self):
-		return self.name.name
+    def __str__(self):
+        return self.name.name
 
 
 
